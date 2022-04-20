9df95ec6effa29555a1d3f68907e12852951bd46
lconnell1000 <lconnell1000@gmail.com>
2022-04-20 15:28:41 +0930

    index for api's done

---
+++ routes/api/index.js
@@ -0,0 +1,7 @@
+const usersRoutes = require("./usersRoutes");
+const thoughtsRoutes = require("./thoughtsRoutes");
+
+router.use("/users", usersRoutes);
+router.use("/thoughts", thoughtsRoutes);
+
+module.exports = router;
